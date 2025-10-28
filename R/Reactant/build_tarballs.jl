21a45a805a2173c7c101dd2e445d128a6c61a0bb
Mosè Giordano <mose@gnu.org>
2025-10-28 22:52:51 +0000

    DROP ME: skip all GPU platforms and debug builds

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -671,6 +671,9 @@ augment_platform_block="""
 
 # for gpu in ("none", "cuda", "rocm"), mode in ("opt", "dbg"), platform in platforms
 for gpu in ("none", "cuda", "rocm"), mode in ("opt", "dbg"), cuda_version in ("none", "12.9", "13.0"), rocm_version in ("none", "7.1",), platform in platforms
+    if !Sys.islinux(platform) || gpu != "none" || mode != "opt"
+        continue
+    end
 
     augmented_platform = deepcopy(platform)
     augmented_platform["mode"] = mode
