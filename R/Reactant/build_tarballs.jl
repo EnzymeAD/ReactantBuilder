32a28e956abda2543d17959115541e7ed7e0f03a
Avik Pal <avik.pal.2017@gmail.com>
2026-02-14 19:47:14 -0500

    Update Reactant commit and version numbers (#97)

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -7,8 +7,8 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-reactant_commit = "0655f3d1bf69cb1cbae6d8a0646e070ae797cd89"
-version = v"0.0.327"
+reactant_commit = "593b068e789a45efb71485a7fdd988f101d0ff91"
+version = v"0.0.328"
 
 sources = [
    GitSource(repo, reactant_commit),
