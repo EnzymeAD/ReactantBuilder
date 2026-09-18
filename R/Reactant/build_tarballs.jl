fe752cc6132b7c57f3764cee334df326c4779f06
William Moses <gh@wsmoses.com>
2026-09-18 12:28:54 +0200

    Update Reactant commit and version to 0.0.409 (#226)

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -7,8 +7,8 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-reactant_commit = "4678655dda2db8af7f8408302e49120ae04a9d44"
-version = v"0.0.408"
+reactant_commit = "349e424fac2380a666d5549397fdd58127e8fc7d"
+version = v"0.0.409"
 
 sources = [
    GitSource(repo, reactant_commit),
